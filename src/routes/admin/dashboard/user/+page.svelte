2cb3a6626a104b220966c96a8a0f3b6e208e9a95
azzahra-04 <nilammagfiraazzahra@gmail.com>
2024-05-21 22:45:30 +0700

    update filtered search admin

--- src/routes/admin/dashboard/user/+page.svelte
+++ src/routes/admin/dashboard/user/+page.svelte
@@ -3,6 +3,16 @@
   /** @type {import('./$types').PageServerData} */
   export let data;
 
+  let searchValue = "";
+  let filteredData = [];
+
+  function handleSearch() {
+    let filter = searchValue.toLowerCase();
+    filteredData = data.users.filter((user) => {
+      return user.fullName.toLowerCase().includes(filter);
+    });
+  }
+
   let openDeleteDialog = false;
   let deleteId = null;
   let deleting = false;
@@ -19,13 +29,12 @@
         <input
           type="text"
           placeholder="Search..."
+          bind:value={searchValue}
           class="border p-2 rounded-s focus:outline-none focus:ring-2 focus:ring-dark-green focus:border-transparent sm:mt-0 md:text-sm"
         />
         <button
           type="button"
-          on:click={() => {
-            console.log("searching...");
-          }}
+          on:click={handleSearch}
           class="grow px-4 py-2 rounded-e border bg-green hover:bg-dark-green sm:mt-0 md:text-sm"
         >
           <i class="fa-solid fa-magnifying-glass"></i>
@@ -48,29 +57,33 @@
         </tr>
       </thead>
       <tbody>
-        {#each data.users as { _id: id, fullName, email, gender, dateOfBirth, telephoneNumber, address }}
-          <tr class="border">
-            <td class="px-6 py-4 border">
-              <span class="block text-sm font-medium">{fullName}</span>
-              <span class="block text-xs">{email}</span>
-            </td>
-            <td class="px-6 py-4 border">{gender}</td>
-            <td class="px-6 py-4 border">{dateOfBirth}</td>
-            <td class="px-6 py-4 border">{telephoneNumber}</td>
-            <td class="px-6 py-4 border">{address}</td>
-            <td class="pl-4 py-4">
-              <button
-                on:click={() => {
-                  openDeleteDialog = true;
-                  deleteId = id;
-                }}
-                class="py-2 px-3 font-medium text-red hover:bg-dark-green rounded"
-              >
-                <i class="fa-solid fa-trash"></i>
-              </button>
-            </td>
-          </tr>
-        {/each}
+        {#if filteredData.length > 0}
+          {#each filteredData as { _id: id, fullName, email, gender, dateOfBirth, telephoneNumber, address }}
+            <tr class="border">
+              <td class="px-6 py-4 border">
+                <span class="block text-sm font-medium">{fullName}</span>
+                <span class="block text-xs">{email}</span>
+              </td>
+              <td class="px-6 py-4 border">{gender}</td>
+              <td class="px-6 py-4 border">{dateOfBirth}</td>
+              <td class="px-6 py-4 border">{telephoneNumber}</td>
+              <td class="px-6 py-4 border">{address}</td>
+              <td class="pl-4 py-4">
+                <button
+                  on:click={() => {
+                    openDeleteDialog = true;
+                    deleteId = id;
+                  }}
+                  class="py-2 px-3 font-medium text-red hover:bg-dark-green rounded"
+                >
+                  <i class="fa-solid fa-trash"></i>
+                </button>
+              </td>
+            </tr>
+          {/each}
+        {:else}
+          <p class="m-4">Tidak ada data yang ditemukan</p>
+        {/if}
       </tbody>
     </table>
   </div>
