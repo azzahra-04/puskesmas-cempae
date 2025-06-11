e6bba66ff1f5a0207571264d90d28790b4ba3257
Zahra <153986893+azzahra-04@users.noreply.github.com>
2025-06-11 09:54:29 +0800

    Update +page.svelte

--- src/routes/(main)/our-services/[booking_room]/+page.svelte
+++ src/routes/(main)/our-services/[booking_room]/+page.svelte
@@ -128,3 +128,15 @@
     </div>
   </button>
 {/if} -->
+
+<section id="not-found">
+  <div class="circles">
+    <p>
+      404<br />
+      <small>PAGE NOT FOUND</small>
+    </p>
+    <span class="circle big"></span>
+    <span class="circle med"></span>
+    <span class="circle small"></span>
+  </div>
+</section>
