9c910810a064e03b1d15126ce902fa83ae972372
Zahra <153986893+azzahra-04@users.noreply.github.com>
2025-06-11 10:49:32 +0800

    Update +page.server.js

--- src/routes/(main)/+page.server.js
+++ src/routes/(main)/+page.server.js
@@ -1,7 +1,9 @@
 import { article } from "$db/collection/article";
+import {error} from '@sveltejs/kit'
 
 /** @type {import('./$types').PageLoad} */
 export async function load() {
+  throw error(404);
   const articles = await article.find({}).toArray();
 
   return {
